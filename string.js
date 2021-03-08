41830d5947579a8b490d3a3c4ac1d762b2cc5a49
bayycesar <74101785+bayycesar@users.noreply.github.com>
2021-03-08 04:22:54 +0200

    update

---
+++ string.js
@@ -0,0 +1,16 @@
+
+
+//using $ symbol
+let myName = "sezer bayram"
+const ces = "I'm " + "Sezer BAYRAM"; 
+const mine = `I'm ${myName}`;
+console.log(mine);
+console.log(ces);
+console.log(`string yazilimlari es6...`);
+
+
+//multible line using
+console.log('sezer bayram\n\
+junior developer\n\
+some works');
+
