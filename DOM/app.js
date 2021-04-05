71f9d78ab2c2754df176ab64fc24e68b9aaf62fa
bayycesar <74101785+bayycesar@users.noreply.github.com>
2021-04-06 02:45:22 +0300

    upload

---
+++ DOM/app.js
@@ -0,0 +1,6 @@
+console.log(document.querySelector('.string').textContent = 'hello world');
+
+document.querySelector('.myBtn').addEventListener('click', function(){
+    console.log(document.querySelector('.string').textContent = ' how are you?');
+})
+  
--- arrayAndObject/app.js
+++ arrayAndObject/app.js
@@ -1,3 +1,9 @@
 const arr = new Array(1,2,3,4,5);
 
 console.log(arr[1]);
+
+
+const sezer = {
+    firstName:sezer,
+    surname:bayraam
+}
